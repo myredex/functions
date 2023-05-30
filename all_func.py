22ccc088c35cb4fd8491e570929042073862451e
Timur Talikbayev <88085889+myredex@users.noreply.github.com>
2023-05-30 11:50:45 +0600

    Update all_func.py

--- all_func.py
+++ all_func.py
@@ -58,3 +58,17 @@ def split_dir_to_train_test_val(directory = "images/",
         shutil.copy2(src=(root+folder+"/"+one_file),
                     dst=(dest_dir+one_file))
       print(f"Folder {folder}. Validation data copied. {len(val_files)} files")
+      
+     
+#################################################################
+
+
+def get_class_names_from_folder(directory):
+  """
+  Get the classnames from train folder for example
+  """
+  import pathlib
+  import numpy as np
+  data_dir = pathlib.Path(directory)
+  class_names = np.array(sorted([item.name for item in data_dir.glob("*")])) # Created a list of class names 
+  print(class_names)
