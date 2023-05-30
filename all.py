35344c865613a60e211f9296181866495a23cad8
Timur Talikbayev <88085889+myredex@users.noreply.github.com>
2023-05-30 11:28:44 +0600

    Create all.py
    Created first function

---
+++ all.py
@@ -0,0 +1,60 @@
+def split_dir_to_train_test_val(directory = "images/",
+                            train_size = 0.7,
+                            test_size = 0.2,
+                            val_size = 0.1):
+  """
+  Creates 3 folders for Train, Test and Validation data
+  """
+  import os
+  import random
+  import shutil
+
+  # Set random seed
+  rng = random.Random(42)
+
+  for root, folders, files in os.walk(directory):
+    for folder in folders:
+      # Create list of the files
+      list_of_files = []
+      for file_name in os.listdir(root+folder+"/"):
+        list_of_files.append(file_name)
+      
+      #  Shuffle the list
+      rng.shuffle(list_of_files)
+
+      # Create lists of files
+      train_files = list_of_files[:int(len(list_of_files)*train_size)]
+      test_files = list_of_files[int(len(list_of_files)*train_size) : int(len(list_of_files)*(train_size+test_size))]
+      val_files = list_of_files[int(len(list_of_files)*(train_size+test_size)):]
+
+      # Create folders and files for train data
+      for one_file in train_files:
+      
+        # Copy  files
+        dest_dir = "files/train/"+folder+"/"
+        os.makedirs(dest_dir, exist_ok=True)
+
+        shutil.copy2(src=(root+folder+"/"+one_file),
+                    dst=(dest_dir+one_file))
+      print(f"Folder {folder}. Train data copied. {len(train_files)} files")
+
+      # Create folders and files for test data
+      for one_file in test_files:      
+        # Copy  files
+        dest_dir = "files/test/"+folder+"/"
+        os.makedirs(dest_dir, exist_ok=True)
+
+        shutil.copy2(src=(root+folder+"/"+one_file),
+                    dst=(dest_dir+one_file))
+      print(f"Folder {folder}. Test data copied. {len(test_files)} files")
+
+      # Create folders and files for validation data
+      for one_file in val_files:
+      
+        # Copy  files
+        dest_dir = "files/validation/"+folder+"/"
+        os.makedirs(dest_dir, exist_ok=True)
+
+        shutil.copy2(src=(root+folder+"/"+one_file),
+                    dst=(dest_dir+one_file))
+      print(f"Folder {folder}. Validation data copied. {len(val_files)} files")
