310b47027014a36e5130dcb06a88fd3d8c5cbc07
Timur Talikbayev <88085889+myredex@users.noreply.github.com>
2024-08-16 15:46:20 +0500

    Update pytorch_helpers_functions.py

--- pytorch_helpers_functions.py
+++ pytorch_helpers_functions.py
@@ -1,6 +1,18 @@
+import numpy as np
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
 # Data Preparation
 # =========================================
 ## calculate_mean_std - calculates mean and std for the images in folder
+
+# Model training
+# =========================================
+## train_step - runs model learning for one epoch
+## val_step - evaluates model for epoch
+## train_model - combines previous two ones
 
 def calculate_mean_std(images_dir:str):
 
@@ -16,11 +28,6 @@ def calculate_mean_std(images_dir:str):
     Example usage:
     mean, std = calculate_mean_std('images')
     """
-
-    from torchvision import datasets
-    from torch.utils.data import DataLoader
-    from torchvision import datasets, transforms
-    import numpy as np
 
     transform = transforms.Compose([transforms.Resize((128, 128)),
                                     transforms.ToTensor()])
@@ -47,3 +54,147 @@ def calculate_mean_std(images_dir:str):
     std /= total_images
     
     return mean, std
+
+# ==============================================================
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device: torch.device):
+
+    """
+    Trains PyTorch model for one epoch.
+    Args:
+    model: PyTorch model
+    dataloader: A DataLoader instance
+    loss_fn: Loss function
+    optimizer: Optimizer
+
+    Returns:
+    A tuple of training loss and accuracy
+    """
+    train_loss, train_acc, train_f1 = 0, 0, 0
+    # Turn on train mode
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+
+        # Send data to device
+        X, y = X.to(device), y.to(device)
+
+        # Forward pass
+        y_pred = model(X)
+        # Calculate the losss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        # Optimizer zero grad
+        optimizer.zero_grad()
+        # Loss backward
+        loss.backward()
+        # Optimizer step
+        optimizer.step()
+
+        # Calculate accuracy
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+        train_f1 += f1_metric(y_pred_class, y)
+
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    train_f1 = train_f1 / len(dataloader)
+
+    # Choose what to return
+    return train_loss, train_f1
+
+# ==============================================================
+def val_step(model: torch.nn.Module,
+             dataloader: torch.utils.data.DataLoader,
+             loss_fn: torch.nn.Module,
+             device: torch.device):
+
+    """
+    Performs PyTorch model's validation during one epoch.
+    Args:
+    model: PyTorch model
+    dataloader: A DataLoader instance
+    loss_fn: Loss function
+    device: Device "cpu" or "cuda"
+
+    Returns:
+    A tuple of val loss and accuracy
+    """
+    val_loss, val_acc, val_f1 = 0, 0, 0
+    # Turn on eval mode
+    model.eval()
+
+    with torch.inference_mode():
+
+        for batch, (X, y) in enumerate(dataloader):
+
+            # Send data to device
+            X, y = X.to(device), y.to(device)
+
+            # Forward pass
+            y_pred = model(X)
+            # Calculate the losss
+            loss = loss_fn(y_pred, y)
+
+            val_loss += loss.item()
+
+            # Calculate accuracy
+            y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+            val_acc += (y_pred_class == y).sum().item()/len(y_pred)
+            val_f1 += f1_metric(y_pred_class, y)
+
+    val_loss = val_loss / len(dataloader)
+    val_acc = val_acc / len(dataloader)
+    val_f1 = val_f1 / len(dataloader)
+
+    # Choose what to return
+    return val_loss, val_f1
+
+# ==============================================================
+def train_model(model: torch.nn.Module,
+                train_dataloader: torch.utils.data.DataLoader,
+                val_dataloader: torch.utils.data.DataLoader,
+                optimizer: torch.optim.Optimizer,
+                loss_fn: torch.nn.Module,
+                epochs: int,
+                device: torch.device):
+
+    """
+    Trains PyTorch model for N epochs
+    Returns:
+    Dictionaries of Train Loss, Val Loss, Train Acc, Val Accuracy.
+    If test dataloader in input it also returns final test Loss and Test accuracy
+    """
+    # Move model to device
+    model.to(device)
+    print(f"Device: {device}")
+
+    train_f1, val_f1, train_loss, val_loss = [], [], [], []
+
+    for epoch in range(epochs):
+        # Run train and collect results in list
+        train_loss_ep, train_f1_ep = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer,
+                                           device=device)
+        train_loss.append(train_loss_ep)
+        train_f1.append(train_f1_ep)
+
+        # Run validation and collect metrics in list
+        val_loss_ep, val_f1_ep = val_step(model=model,
+                                     dataloader=val_dataloader,
+                                     loss_fn=loss_fn,
+                                     device=device)
+        val_loss.append(val_loss_ep)
+        val_f1.append(val_f1_ep)
+
+        print(f"""Epoch {epoch+1} of {epochs}_ _ _ _ _ _ _ _ _ _
+              Train loss: {train_loss_ep:.5f}
+              Val   loss: {val_loss_ep:.5f}
+              Train f1: {train_f1_ep:.5f}
+              Val   f1: {val_f1_ep:.5f}""")
+
+    return train_f1, val_f1, train_loss, val_loss
