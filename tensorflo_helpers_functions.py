a81043fcfa08d0cd6da2a7b237e9074c87fdd899
Timur Talikbayev <88085889+myredex@users.noreply.github.com>
2024-09-11 10:00:39 +0500

    Update tensorflo_helpers_functions.py

--- tensorflo_helpers_functions.py
+++ tensorflo_helpers_functions.py
@@ -2,13 +2,6 @@
 # =========================================
 # plot_dcm_image
 
-
-# Work with Files
-# =========================================
-# unzip_data - Unzips archive
-# walk_through_dir - Counts files and subdirs in directory
-# split_dir_to_train_test_val - Splits one folder with images into three subfolders for tran, test and val
-
 # Plots of curves
 # =========================================
 # get_class_names_from_folder
@@ -52,70 +45,6 @@ def plot_dcm_image(image_path:str, fig_size=(3, 3)):
     # Print info
     print(f"Image size: {image.shape}")
     print(f"Image url: {image_path}")
-
-
-
-def split_dir_to_train_test_val(directory = "images/",
-                            train_size = 0.7,
-                            test_size = 0.2,
-                            val_size = 0.1):
-  """
-  Creates 3 folders for Train, Test and Validation data
-  """
-  import tensorflow as tf
-  import random
-  import shutil
-
-  # Set random seed
-  rng = random.Random(42)
-
-  for root, folders, files in os.walk(directory):
-    for folder in folders:
-      # Create list of the files
-      list_of_files = []
-      for file_name in os.listdir(root+folder+"/"):
-        list_of_files.append(file_name)
-      
-      #  Shuffle the list
-      rng.shuffle(list_of_files)
-
-      # Create lists of files
-      train_files = list_of_files[:int(len(list_of_files)*train_size)]
-      test_files = list_of_files[int(len(list_of_files)*train_size) : int(len(list_of_files)*(train_size+test_size))]
-      val_files = list_of_files[int(len(list_of_files)*(train_size+test_size)):]
-
-      # Create folders and files for train data
-      for one_file in train_files:
-      
-        # Copy  files
-        dest_dir = "files/train/"+folder+"/"
-        os.makedirs(dest_dir, exist_ok=True)
-
-        shutil.copy2(src=(root+folder+"/"+one_file),
-                    dst=(dest_dir+one_file))
-      print(f"Folder {folder}. Train data copied. {len(train_files)} files")
-
-      # Create folders and files for test data
-      for one_file in test_files:      
-        # Copy  files
-        dest_dir = "files/test/"+folder+"/"
-        os.makedirs(dest_dir, exist_ok=True)
-
-        shutil.copy2(src=(root+folder+"/"+one_file),
-                    dst=(dest_dir+one_file))
-      print(f"Folder {folder}. Test data copied. {len(test_files)} files")
-
-      # Create folders and files for validation data
-      for one_file in val_files:
-      
-        # Copy  files
-        dest_dir = "files/validation/"+folder+"/"
-        os.makedirs(dest_dir, exist_ok=True)
-
-        shutil.copy2(src=(root+folder+"/"+one_file),
-                    dst=(dest_dir+one_file))
-      print(f"Folder {folder}. Validation data copied. {len(val_files)} files")
-      
      
 
 
@@ -357,43 +286,7 @@ def compare_historys(original_history, new_history, initial_epochs=5):
     plt.xlabel('epoch')
     plt.show()
   
-# Create function to unzip a zipfile into current working directory 
-# (since we're going to be downloading and unzipping a few files)
 
-def unzip_data(filename):
-    """
-    Unzips filename into the current working directory then removes .zip file.
-
-    Args:
-    filename (str): a filepath to a target zip folder to be unzipped.
-    """
-    zip_ref = ZipFile(filename, "r")
-    print("Unzipping")
-    zip_ref.extractall()
-    zip_ref.close()
-
-    os.remove(filename) # Remove source file
-    print(f"{filename} Extracted successfully")
-
-# Walk through an image classification directory and find out how many files (images)
-# are in each subdirectory.
-
-def walk_through_dir(dir_path):
-  """
-  Walks through dir_path returning its contents.
-
-  Args:
-    dir_path (str): target directory
-  
-  Returns:
-    A print out of:
-      number of subdiretories in dir_path
-      number of images (files) in each subdirectory
-      name of each subdirectory
-  """
-  for dirpath, dirnames, filenames in os.walk(dir_path):
-    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
-    
 # Function to evaluate: accuracy, precision, recall, f1-score
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
